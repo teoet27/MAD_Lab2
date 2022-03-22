320211f936709214f39b98be09cbded8d864d920
lifeofvins <vincenzo.sagristano@yahoo.it>
2022-03-22 15:06:46 +0100

    fixed: main class has to be named ShowProfileActivity.kt

--- app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
+++ app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
@@ -3,7 +3,7 @@ package com.example.mad_lab2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
