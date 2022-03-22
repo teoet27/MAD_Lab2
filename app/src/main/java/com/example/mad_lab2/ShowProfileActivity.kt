b88d1c22e3e512e93cc6b7607c286744f030257a
lifeofvins <vincenzo.sagristano@yahoo.it>
2022-03-22 16:11:10 +0100

    added: new icon for the project

--- app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
+++ app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
@@ -2,10 +2,12 @@ package com.example.mad_lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
 }
