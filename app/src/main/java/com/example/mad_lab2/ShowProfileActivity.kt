33e1378f1bc1372c5bc6bec08b3ffb04c561d6e9
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-03-25 17:27:04 +0100

    removed: useless import

--- app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
+++ app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
