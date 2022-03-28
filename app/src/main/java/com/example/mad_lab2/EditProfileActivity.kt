9533d90f44a489edc2413440dd9ce4b6777e70f4
Matteo <teoet27@gmail.com>
2022-03-28 18:24:30 +0200

    initial image management

--- app/src/main/java/com/example/mad_lab2/EditProfileActivity.kt
+++ app/src/main/java/com/example/mad_lab2/EditProfileActivity.kt
@@ -1,21 +1,33 @@
 package com.example.mad_lab2
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.*
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditProfileActivity : AppCompatActivity() {
     private lateinit var editFullNameOBJ: EditText
     private lateinit var editNickNameOBJ: EditText
     private lateinit var editQualificationOBJ: EditText
     private lateinit var sdh: SaveProfileDataHandler
+    lateinit var currentPhotoPath: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +49,87 @@ class EditProfileActivity : AppCompatActivity() {
         this.editFullNameOBJ.setText(intent.getCharSequenceExtra("fullname"))
         this.editNickNameOBJ.setText(intent.getCharSequenceExtra("nickname"))
         this.editQualificationOBJ.setText(intent.getCharSequenceExtra("qualification"))
+
+        val cam: ImageView=findViewById(R.id.camera_button)
+        registerForContextMenu(cam)
+        cam.setOnClickListener{openContextMenu(cam)}
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        if(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY))
+            inflater.inflate(R.menu.select_take_picture_floating_context_menu, menu)
+        else
+            inflater.inflate(R.menu.select_picture_floating_context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.title){
+            resources.getString(R.string.floating_menu_shot_picture) -> dispatchTakePictureIntent()
+            resources.getString(R.string.floating_menu_load_picture) -> dispatchLoadPictureIntent()
+        }
+        return true
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    val PICK_IMAGE = 100
+    private fun dispatchLoadPictureIntent() {
+        val loadPictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        try {
+            startActivityForResult(loadPictureIntent, PICK_IMAGE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            findViewById<ImageView>(R.id.edit_user_image).setImageBitmap(imageBitmap)
+            //save image in proper location and load on ShowProfileActivity
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
+++ app/src/main/java/com/example/mad_lab2/ShowProfileActivity.kt
@@ -4,9 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.view.*
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
