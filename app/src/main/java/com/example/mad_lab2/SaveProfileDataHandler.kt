688da0015a192ea5559178deb53b3e20746e4e5b
Vincenzo Sagristano <80033167+lifeofvins@users.noreply.github.com>
2022-03-29 10:12:46 +0200

    fixed: Gson library for JSON conversion of object

--- app/src/main/java/com/example/mad_lab2/SaveProfileDataHandler.kt
+++ app/src/main/java/com/example/mad_lab2/SaveProfileDataHandler.kt
@@ -1,30 +1,17 @@
 package com.example.mad_lab2
 
-import org.json.JSONObject
 import android.content.Context
+import com.google.gson.Gson
 
 class SaveProfileDataHandler(context: Context) {
-    private val jsonObject = JSONObject()
     private val sharedPrefereces =
         context.getSharedPreferences("com.example.mad_lab2_profileData", Context.MODE_PRIVATE)
 
-    private fun putIntoJSON(profile: Profile?) {
-        if (profile != null)
-            this.jsonObject.put("profile", profile)
-    }
-
-    private fun getFromJSON(): Profile? {
-        return this.jsonObject.get("profile") as Profile?
-    }
-
     public fun storeData(profile: Profile?) {
-        putIntoJSON(profile)
-        sharedPrefereces.edit().putString("profile", jsonObject.toString()).apply()
+        sharedPrefereces.edit().putString("profile", Gson().toJson(profile)).apply()
     }
 
     public fun retrieveData(): Profile? {
-        val tmp = sharedPrefereces.getString("profile", null) ?: return null
-        jsonObject.put("profile", JSONObject(tmp))
-        return getFromJSON()
+        return Gson().fromJson(sharedPrefereces.getString("profile", null),Profile::class.java)?:null
     }
 }
