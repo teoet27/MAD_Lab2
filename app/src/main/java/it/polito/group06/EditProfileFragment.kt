7713a9b68a9b623d9e64a6c7ef4d8687028d68b1
annaoriglia <48447099+annaoriglia@users.noreply.github.com>
2022-05-03 11:29:07 +0200

    working navigation drawer, working fragments and navigation graph, working back button. Menu buttons not working yet. Edit profile fragment added

--- app/src/main/java/it/polito/group06/EditProfileFragment.kt
+++ app/src/main/java/it/polito/group06/EditProfileFragment.kt
@@ -1,23 +1,144 @@
 package it.polito.group06
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.os.Vibrator
+import android.provider.MediaStore
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import it.polito.group06.utilities.createImageFile
+import java.io.File
+import java.io.IOException
 
+class EditProfileFragment : Fragment() {
+    private lateinit var editFullNameOBJ: EditText
+    private lateinit var editNickNameOBJ: EditText
+    private lateinit var editQualificationOBJ: EditText
+    private lateinit var editDescriptionOBJ: EditText
+    private lateinit var editEmailOBJ: EditText
+    private lateinit var editLocationOBJ: EditText
+    private lateinit var editSkillsOBJ: EditText
+    private lateinit var editPhoneOBJ: EditText
+    private lateinit var sdh: SaveProfileDataHandler
+    private lateinit var vibrator: Vibrator
+    private lateinit var profilePictureOBJ: ImageView
+    private lateinit var photoURI: Uri
+    private lateinit var profilePictureDirectoryPath: String
+    private lateinit var profilePicturePath: String
 
-class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val PICK_IMAGE = 100
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+
+        val camera_button = view.findViewById<Button>(R.id.edit_camera_button)
+        registerForContextMenu(camera_button)
+
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.action_frag4EditTimeslot_to_frag3ShowTimeslot)
+            }
+        })
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                findNavController().navigate(R.id.action_editProfileFragment_to_frag1ShowProfile2)
+    }
+
+    /**
+     * This method invokes an intent to take a picture. First, it checks if there is an
+     * available camera, then it creates a file where to store the image and finally the intent
+     * is dispatched.
+     */
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(view?.context!!.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile(profilePicturePath)
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    ex.printStackTrace()
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    photoURI = FileProvider.getUriForFile(
+                        requireView().context,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
             }
-        })
+        }
+    }
+
+    /**
+     * This method invokes an intent to load a picture from the filesystem
+     */
+    private fun dispatchLoadPictureIntent() {
+        val loadPictureIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        try {
+            startActivityForResult(loadPictureIntent, PICK_IMAGE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * This method chooses a menu to be inflated on the basis of the availability of the camera
+     *
+     * @param menu  ContextMenu
+     * @param v  View
+     * @param menuInfo  ContextMenu info
+     * @return true
+     */
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = requireActivity().menuInflater
+        if (view?.context!!.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY))
+            inflater.inflate(R.menu.select_take_picture_floating_context_menu, menu)
+        else
+            inflater.inflate(R.menu.select_picture_floating_context_menu, menu)
+    }
+
+    /**
+     * This method invokes an intent for either taking or loading a picture
+     *
+     * @param item  Chosen menu entry
+     * @return true
+     */
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.title) {
+            resources.getString(R.string.floating_menu_shot_picture) -> dispatchTakePictureIntent()
+            resources.getString(R.string.floating_menu_load_picture) -> dispatchLoadPictureIntent()
+        }
+        return true
     }
 }
