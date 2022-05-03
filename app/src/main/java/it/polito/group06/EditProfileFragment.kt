55a66f05d5edb7203b9f466aa3c97fd0dca9bd60
annaoriglia <48447099+annaoriglia@users.noreply.github.com>
2022-05-03 11:25:54 +0200

    working navigation drawer, working fragments and navigation graph, working back button. Menu buttons not working yet. Edit profile fragment added

--- app/src/main/java/it/polito/group06/EditProfileFragment.kt
+++ app/src/main/java/it/polito/group06/EditProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 
 
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
@@ -12,5 +14,10 @@ class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.action_editProfileFragment_to_frag1ShowProfile2)
+            }
+        })
     }
 }
