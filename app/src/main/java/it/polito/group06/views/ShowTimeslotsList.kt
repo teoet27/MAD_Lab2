e86555727eed3bafc7df643372e4eed2343d5079
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-06 22:03:17 +0200

    fixed: create_hint is visible only if the list is empty

--- app/src/main/java/it/polito/group06/views/ShowTimeslotsList.kt
+++ app/src/main/java/it/polito/group06/views/ShowTimeslotsList.kt
@@ -1,6 +1,5 @@
 package it.polito.group06.views
 
-import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,31 +8,33 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.viewModels
 import it.polito.group06.R
+import it.polito.group06.viewmodels.TimeSlotAdViewModel
 
-class ShowTimeslotsList: Fragment(R.layout.show_timeslots_frag) {
+class ShowTimeslotsList : Fragment(R.layout.show_timeslots_frag) {
 
-    private lateinit var defaultText: TextView
+    private val timeslotsVM by viewModels<TimeSlotAdViewModel>()
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?):View?{
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         return inflater.inflate(
             R.layout.show_timeslots_frag,
             container,
-            false);
+            false
+        );
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val tmpList = mutableListOf<String>("ciao", "sono", "vivi")
-        val rv = view.findViewById<RecyclerView>(R.id.rvAdvFullList)
-        this.defaultText = view.findViewById(R.id.defaultTextTimeslotsList)
-        if(true) {
-            this.defaultText.isVisible = true
-            view.findViewById<ImageView>(R.id.create_hint).isVisible=true
+        // timeslotsVM.insertAd(TimeSlotAd(null, "Ciao", "x", "x", "x"))
+        timeslotsVM.ads.observe(this.viewLifecycleOwner) { listOfAdv ->
+            view.findViewById<TextView>(R.id.defaultTextTimeslotsList).isVisible = listOfAdv == null || listOfAdv.isEmpty()
+            view.findViewById<ImageView>(R.id.create_hint).isVisible = listOfAdv == null || listOfAdv.isEmpty()
         }
     }
 }
