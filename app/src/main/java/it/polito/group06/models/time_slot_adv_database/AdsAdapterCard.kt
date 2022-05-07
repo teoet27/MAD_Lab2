a000c4efcd27f213b120753a7aacef144624d328
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-07 19:10:16 +0200

    feat: setting of the viewModel

--- app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterCard.kt
+++ app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterCard.kt
@@ -2,13 +2,15 @@ package it.polito.group06.models.time_slot_adv_database
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import it.polito.group06.R
+import it.polito.group06.viewmodels.AdvertisementViewModel
 
 /**
  * AdsAdapter extends the Adapter of the [RecyclerView] and implements the required methods.
  */
-class AdsAdapterCard(val adsList: List<Advertisement>) : RecyclerView.Adapter<AdsViewHolderCard>() {
+class AdsAdapterCard(val adsList: List<Advertisement>, val advViewModel: AdvertisementViewModel) : RecyclerView.Adapter<AdsViewHolderCard>() {
 
     /**
      * On creating the view holder, based on whether the ads is private or not, it'll be created a regular timeslot layout fragment or an empty one.
@@ -24,6 +26,10 @@ class AdsAdapterCard(val adsList: List<Advertisement>) : RecyclerView.Adapter<Ad
      */
     override fun onBindViewHolder(holder: AdsViewHolderCard, position: Int) {
         holder.bind(adsList[position])
+        holder.itemView.setOnClickListener {
+            advViewModel.singleAdvertisement = adsList[position]
+            Navigation.findNavController(it).navigate(R.id.action_ShowListTimeslots_to_showSingleTimeslot)
+        }
     }
 
     /**
