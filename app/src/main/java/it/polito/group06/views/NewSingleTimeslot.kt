182482ef6ec850fa34cbdb60223e914eb0b640e1
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-07 14:58:59 +0200

    feat: clicking on the "X" it's now possible to go back without saving the advertisement.

--- app/src/main/java/it/polito/group06/views/NewSingleTimeslot.kt
+++ app/src/main/java/it/polito/group06/views/NewSingleTimeslot.kt
@@ -2,6 +2,8 @@ package it.polito.group06.views
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
@@ -21,6 +23,7 @@ class NewSingleTimeslot : Fragment(R.layout.new_time_slot_details_fragment) {
     private lateinit var newDate: TextView
     private lateinit var newDuration: TextView
     private lateinit var newDescription: TextView
+    private lateinit var closeButton: ImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,23 +33,43 @@ class NewSingleTimeslot : Fragment(R.layout.new_time_slot_details_fragment) {
         this.newDate = view.findViewById(R.id.newDate)
         this.newDuration = view.findViewById(R.id.newDuration)
         this.newDescription = view.findViewById(R.id.newDescription)
+        this.closeButton = view.findViewById(R.id.closeButton)
+
+        this.closeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_newTimeSlotDetailsFragment_to_ShowListTimeslots)
+        }
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                advViewModel.insertAd(
-                    Advertisement(
-                        null,
-                        newTitle.text.toString(),
-                        newDescription.text.toString(),
-                        newLocation.text.toString(),
-                        newDate.text.toString(),
-                        0f,
-                        "Guido Saracco",
-                        false
+                if(isAdvAvailable()) {
+                    advViewModel.insertAd(
+                        Advertisement(
+                            null,
+                            newTitle.text.toString(),
+                            newDescription.text.toString(),
+                            newLocation.text.toString(),
+                            newDate.text.toString(),
+                            0f,
+                            "Guido Saracco",
+                            false
+                        )
                     )
-                )
+                }
                 findNavController().navigate(R.id.action_newTimeSlotDetailsFragment_to_ShowListTimeslots)
             }
         })
+    }
+
+    /**
+     * TODO: implement this function based on this class attributes
+     *
+     * isAdvAvailable is a method which returns whether it's possible to actually insert a new
+     * advertisement. The criteria is that an advertisement should at least have a title, a location,
+     * a date and a duration.
+     *
+     * @return whether it's possible to actually create an advertisement or not
+     */
+    private fun isAdvAvailable(): Boolean {
+        return true
     }
 }
