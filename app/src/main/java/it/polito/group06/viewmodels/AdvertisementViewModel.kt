8c6f185e0198fcea2ade3e10c2bb8cdc52767d0b
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-08 18:52:33 +0200

    improved: now there's an observable LiveData for a single advertisement and a proper method to update its value

--- app/src/main/java/it/polito/group06/viewmodels/AdvertisementViewModel.kt
+++ app/src/main/java/it/polito/group06/viewmodels/AdvertisementViewModel.kt
@@ -2,11 +2,16 @@ package it.polito.group06.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import it.polito.group06.models.time_slot_adv_database.Advertisement
 import it.polito.group06.models.time_slot_adv_database.AdvertisementRepository
 import kotlin.concurrent.thread
 
 class AdvertisementViewModel(application: Application) : AndroidViewModel(application) {
+    /**
+     * Repository
+     */
     private val repo = AdvertisementRepository(application)
 
     /**
@@ -15,16 +20,20 @@ class AdvertisementViewModel(application: Application) : AndroidViewModel(applic
     val ads = repo.advertisements()
 
     /**
-     * Single advertisement
+     * Single [Advertisement]
      */
-    var singleAdvertisement = Advertisement(
+    private var _singleAdvertisementPH = Advertisement(
         null, "", "",
         "", "", 0f,
         "", false
     )
+    private val _pvtAdvertisement = MutableLiveData<Advertisement>().also {
+        it.value = _singleAdvertisementPH
+    }
+    val advertisement: LiveData<Advertisement> = this._pvtAdvertisement
 
     /**
-     * Insertion of a new advertisement
+     * Insertion of a new [Advertisement]
      * @param ad a new advertisement
      */
     fun insertAd(ad: Advertisement) {
@@ -35,7 +44,7 @@ class AdvertisementViewModel(application: Application) : AndroidViewModel(applic
 
     /**
      * Remove an advertisement
-     * @param id integer unique number identifying an advertisement
+     * @param id integer unique number identifying an [Advertisement]
      */
     fun removeAd(id: Long) {
         thread {
@@ -50,5 +59,16 @@ class AdvertisementViewModel(application: Application) : AndroidViewModel(applic
         thread {
             repo.clearAll()
         }
+    }
+
+    /**
+     * updateAdv is a method to update the [_pvtAdvertisement] and it's called by the adapter to
+     * trigger all the observators.
+     * @param newAdv an object of class [Advertisement] which contains all the information to fill
+     * the advertisement view.
+     */
+    fun updateAdv(newAdv: Advertisement) {
+        this._singleAdvertisementPH = newAdv
+        this._pvtAdvertisement.value = _singleAdvertisementPH
     }
 }
