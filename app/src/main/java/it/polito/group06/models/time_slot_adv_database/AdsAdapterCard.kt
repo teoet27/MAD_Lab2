d893a7cbeeba6b4ef6d4933ea2451cf0e70e967d
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-08 18:49:25 +0200

    fixed: the callback calls now a proper function to set the LiveData

--- app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterCard.kt
+++ app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterCard.kt
@@ -8,13 +8,11 @@ import it.polito.group06.R
 import it.polito.group06.viewmodels.AdvertisementViewModel
 
 /**
- * AdsAdapter extends the Adapter of the [RecyclerView] and implements the required methods.
+ * [AdsAdapterCard] extending the Adapter of the [RecyclerView] and implements the required methods.
  */
-class AdsAdapterCard(private val adsList: List<Advertisement>, private val advViewModel: AdvertisementViewModel) : RecyclerView.Adapter<AdsViewHolderCard>() {
+class AdsAdapterCard(private val adsList: List<Advertisement>,
+                     private val advViewModel: AdvertisementViewModel) : RecyclerView.Adapter<AdsViewHolderCard>() {
 
-    /**
-     * On creating the view holder, based on whether the ads is private or not, it'll be created a regular timeslot layout fragment or an empty one.
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdsViewHolderCard {
         val inflater = LayoutInflater.from(parent.context)
         val v = inflater.inflate(R.layout.adv_item, parent, false)
@@ -26,9 +24,9 @@ class AdsAdapterCard(private val adsList: List<Advertisement>, private val advVi
      */
     override fun onBindViewHolder(holder: AdsViewHolderCard, position: Int) {
         holder.bind(adsList[position])
-        holder.itemView.setOnClickListener {
-            advViewModel.singleAdvertisement = adsList[position]
-            Navigation.findNavController(it).navigate(R.id.action_ShowListTimeslots_to_showSingleTimeslot)
+        holder.itemView.setOnClickListener { view ->
+            advViewModel.updateAdv((adsList[adsList.indexOf(adsList[position])]))
+            Navigation.findNavController(view).navigate(R.id.action_ShowListTimeslots_to_showSingleTimeslot)
         }
     }
 
