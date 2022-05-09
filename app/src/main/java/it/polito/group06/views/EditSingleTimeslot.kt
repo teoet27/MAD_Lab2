213b1d9d46cda93fb08f24b05c316bae5c75397d
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-09 18:58:57 +0200

    added: datePicker in the editMode

--- app/src/main/java/it/polito/group06/views/EditSingleTimeslot.kt
+++ app/src/main/java/it/polito/group06/views/EditSingleTimeslot.kt
@@ -2,6 +2,7 @@ package it.polito.group06.views
 
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
@@ -12,6 +13,8 @@ import it.polito.group06.R
 import it.polito.group06.models.time_slot_adv_database.Advertisement
 import it.polito.group06.viewmodels.AdvertisementViewModel
 import it.polito.group06.viewmodels.UserProfileViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditSingleTimeslot : Fragment(R.layout.edit_time_slot_details_fragment) {
 
@@ -25,21 +28,22 @@ class EditSingleTimeslot : Fragment(R.layout.edit_time_slot_details_fragment) {
 
     private lateinit var advTitle: TextView
     private lateinit var advLocation: TextView
-    private lateinit var advDate: TextView
     private lateinit var advDuration: TextView
     private lateinit var advDescription: TextView
     private lateinit var deleteButton: ImageView
     private lateinit var accountName: String
+    private lateinit var datePicker: DatePicker
+    private lateinit var chosenDate: String
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         this.advTitle = view.findViewById(R.id.newTitle)
         this.advLocation = view.findViewById(R.id.newLocation)
-        this.advDate = view.findViewById(R.id.newDate)
         this.advDuration = view.findViewById(R.id.newDuration)
         this.advDescription = view.findViewById(R.id.newDescription)
         this.deleteButton = view.findViewById(R.id.deleteButton)
+        this.datePicker = view.findViewById(R.id.newDateDP)
 
         usrViewModel.profile.observe(viewLifecycleOwner) { user ->
             accountName = if (user?.fullName == null) "Guido Saracco" else user.fullName!!
@@ -50,7 +54,21 @@ class EditSingleTimeslot : Fragment(R.layout.edit_time_slot_details_fragment) {
             dumbAdvertisement.advAccount = singleAdvertisement.advAccount
             this.advTitle.text = singleAdvertisement.advTitle
             this.advLocation.text = singleAdvertisement.advLocation
-            this.advDate.text = singleAdvertisement.advDate
+
+            val loadedDate = Calendar.getInstance()
+            loadedDate.set(
+                singleAdvertisement.advDate.split("/")[2].toInt(),
+                singleAdvertisement.advDate.split("/")[1].toInt() - 1,
+                singleAdvertisement.advDate.split("/")[0].toInt()
+            )
+            chosenDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())
+            datePicker.init(
+                loadedDate.get(Calendar.YEAR), loadedDate.get(Calendar.MONTH),
+                loadedDate.get(Calendar.DAY_OF_MONTH)
+            ) { view, year, month, day ->
+                chosenDate = "$day/${month + 1}/$year"
+            }
+
             this.advDuration.text = singleAdvertisement.advDuration.toString()
             this.advDescription.text = singleAdvertisement.advDescription
             this.deleteButton.setOnClickListener {
@@ -64,7 +82,7 @@ class EditSingleTimeslot : Fragment(R.layout.edit_time_slot_details_fragment) {
                 dumbAdvertisement.advTitle = advTitle.text.toString()
                 dumbAdvertisement.advLocation = advLocation.text.toString()
                 dumbAdvertisement.advDescription = advDescription.text.toString()
-                dumbAdvertisement.advDate = advDate.text.toString()
+                dumbAdvertisement.advDate = chosenDate
                 dumbAdvertisement.advDuration = advDuration.text.toString().toFloat()
                 advViewModel.editSingleAdvertisement(dumbAdvertisement)
                 findNavController().navigate(R.id.action_editTimeSlotDetailsFragment_to_showSingleTimeslot)
