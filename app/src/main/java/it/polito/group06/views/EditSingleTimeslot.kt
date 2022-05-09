5aecf7436be42848f2356860f610b71fe2937293
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-09 17:15:58 +0200

    added: dumbAdvertisement to be passed to the update

--- app/src/main/java/it/polito/group06/views/EditSingleTimeslot.kt
+++ app/src/main/java/it/polito/group06/views/EditSingleTimeslot.kt
@@ -9,13 +9,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import it.polito.group06.R
+import it.polito.group06.models.time_slot_adv_database.Advertisement
 import it.polito.group06.viewmodels.AdvertisementViewModel
 import it.polito.group06.viewmodels.UserProfileViewModel
 
-class EditSingleTimeslot: Fragment(R.layout.edit_time_slot_details_fragment) {
+class EditSingleTimeslot : Fragment(R.layout.edit_time_slot_details_fragment) {
 
     private val advViewModel: AdvertisementViewModel by activityViewModels<AdvertisementViewModel>()
     private val usrViewModel: UserProfileViewModel by activityViewModels<UserProfileViewModel>()
+    private val dumbAdvertisement: Advertisement = Advertisement(
+        null, "", "",
+        "", "", 0f,
+        "", false
+    )
 
     private lateinit var advTitle: TextView
     private lateinit var advLocation: TextView
@@ -36,15 +42,20 @@ class EditSingleTimeslot: Fragment(R.layout.edit_time_slot_details_fragment) {
         this.deleteButton = view.findViewById(R.id.deleteButton)
 
         usrViewModel.profile.observe(viewLifecycleOwner) { user ->
-            accountName = user.fullName!!
+            if (user == null || user.fullName == null)
+                accountName = "Guido Saracco"
+            else
+                accountName = user.fullName!!
         }
 
         advViewModel.advertisement.observe(viewLifecycleOwner) { singleAdvertisement ->
-            this.advTitle.text = singleAdvertisement.adsTitle
-            this.advLocation.text = singleAdvertisement.adsLocation
-            this.advDate.text = singleAdvertisement.adsDate
-            this.advDuration.text = singleAdvertisement.adsDuration.toString()
-            this.advDescription.text = singleAdvertisement.adsDescription
+            dumbAdvertisement.id = singleAdvertisement.id
+            dumbAdvertisement.advAccount = singleAdvertisement.advAccount
+            this.advTitle.text = singleAdvertisement.advTitle
+            this.advLocation.text = singleAdvertisement.advLocation
+            this.advDate.text = singleAdvertisement.advDate
+            this.advDuration.text = singleAdvertisement.advDuration.toString()
+            this.advDescription.text = singleAdvertisement.advDescription
             this.deleteButton.setOnClickListener {
                 advViewModel.removeAd(singleAdvertisement.id!!)
                 findNavController().navigate(R.id.action_editTimeSlotDetailsFragment_to_ShowListTimeslots)
@@ -53,7 +64,12 @@ class EditSingleTimeslot: Fragment(R.layout.edit_time_slot_details_fragment) {
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                // save the data
+                dumbAdvertisement.advTitle = advTitle.text.toString()
+                dumbAdvertisement.advLocation = advLocation.text.toString()
+                dumbAdvertisement.advDescription = advDescription.text.toString()
+                dumbAdvertisement.advDate = advDate.text.toString()
+                dumbAdvertisement.advDuration = advDuration.text.toString().toFloat()
+                advViewModel.editSingleAdvertisement(dumbAdvertisement)
                 findNavController().navigate(R.id.action_editTimeSlotDetailsFragment_to_showSingleTimeslot)
             }
         })
