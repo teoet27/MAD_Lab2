b997dd3658369a05437ff807b2b8ec4563b436e7
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-09 12:02:48 +0200

    added: callback to remove item

--- app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterEdit.kt
+++ app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsAdapterEdit.kt
@@ -2,8 +2,10 @@ package it.polito.group06.models.time_slot_adv_database
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import it.polito.group06.R
+import it.polito.group06.viewmodels.AdvertisementViewModel
 
 /**
  * AdsAdapter extends the Adapter of the [RecyclerView] and implements the required methods.
@@ -13,17 +15,21 @@ class AdsAdapterEdit(private val adsList: List<Advertisement>, private val adver
     /**
      * On creating the view holder, based on whether the ads is private or not, it'll be created a regular timeslot layout fragment or an empty one.
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdsViewHolderExpanded {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdsViewHolderEdit {
         val inflater = LayoutInflater.from(parent.context)
         val v = inflater.inflate(R.layout.adv_item, parent, false)
-        return AdsViewHolderExpanded(v)
+        return AdsViewHolderEdit(v)
     }
 
     /**
      * Bind operations.
      */
-    override fun onBindViewHolder(holder: AdsViewHolderExpanded, position: Int) {
-        holder.bind(adsList[position]) {}
+    override fun onBindViewHolder(holder: AdsViewHolderEdit, position: Int) {
+        holder.bind(adsList[position]) {
+            advertisementViewModel.removeAd(adsList.indexOf(adsList[position]).toLong())
+            notifyItemRemoved(adsList.indexOf(adsList[position]))
+            Navigation.findNavController(holder.itemView).navigate(R.id.action_editTimeSlotDetailsFragment_to_ShowListTimeslots)
+        }
     }
 
     /**
@@ -37,7 +43,7 @@ class AdsAdapterEdit(private val adsList: List<Advertisement>, private val adver
      * OnViewRecycled invokes the unbind method to set on null the callback to the listener
      * of the moreButton
      */
-    override fun onViewRecycled(holder: AdsViewHolderExpanded) {
+    override fun onViewRecycled(holder: AdsViewHolderEdit) {
         holder.unbind()
     }
 }
--- app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsViewHolderEdit.kt
+++ app/src/main/java/it/polito/group06/models/time_slot_adv_database/AdsViewHolderEdit.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import it.polito.group06.R
 
 /**
- * [AdsViewHolderExpanded] extends the ViewHolder of the [RecyclerView]
+ * [AdsViewHolderEdit] extends the ViewHolder of the [RecyclerView]
  * and provides the references to each component of the advertisement
  * card.
  */
@@ -18,22 +18,22 @@ class AdsViewHolderEdit(v: View) : RecyclerView.ViewHolder(v) {
     private val date: TextView = v.findViewById(R.id.newDate)
     private val duration: TextView = v.findViewById(R.id.newDuration)
     private val account: TextView = v.findViewById(R.id.ads_account)
-    private val moreButton: ImageView = v.findViewById(R.id.moreButtonID)
+    private val removeButton: ImageView = v.findViewById(R.id.deleteButton)
 
     /**
      * bind:
      * A method to bind the i-th entry of the adsList to the i-th holder properties.
      * @param adv an object of class Advertisement
-     * @param moreButtonAction the callback to be set at the moreButton listener
+     * @param removeButtonAction the callback to be set at the removeButton listener
      */
-    fun bind(adv: Advertisement, moreButtonAction: () -> Unit) {
+    fun bind(adv: Advertisement, removeButtonAction: () -> Unit) {
         this.title.text = adv.adsTitle
         this.description.text = adv.adsDescription
         this.location.text = adv.adsLocation
         this.date.text = adv.adsDate
         this.duration.text = adv.adsDuration.toString()
         this.account.text = adv.adsAccount
-        this.moreButton.setOnClickListener { moreButtonAction }
+        this.removeButton.setOnClickListener { removeButtonAction }
     }
 
     /**
@@ -41,6 +41,6 @@ class AdsViewHolderEdit(v: View) : RecyclerView.ViewHolder(v) {
      * A method to provide safeness when recycling a component in a RecyclerView
      */
     fun unbind() {
-        moreButton.setOnClickListener(null)
+        removeButton.setOnClickListener(null)
     }
 }
