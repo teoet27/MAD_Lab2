78177b208e8b47ce2645cef6d8d5ac58829efdb6
annaoriglia <48447099+annaoriglia@users.noreply.github.com>
2022-05-15 12:03:52 +0200

    Added methods to login to Google in login activity, server ID to strings, objet to store preferences

--- app/src/main/java/it/polito/group06/GoogleLoginActivity.kt
+++ app/src/main/java/it/polito/group06/GoogleLoginActivity.kt
@@ -1,11 +1,90 @@
 package it.polito.group06
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import it.polito.group06.utilities.GoogleLoginSavedPreferencesObject
+import it.polito.group06.views.TBMainActivity
 
 class GoogleLoginActivity : AppCompatActivity() {
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code:Int=123
+    private var firebaseAuth= FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_login)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // getting the value of gso inside the GoogleSigninClient
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
+        // initialize the firebaseAuth variable
+        firebaseAuth = FirebaseAuth.getInstance()
     }
+
+
+    // signInGoogle() function
+    private  fun signInGoogle(){
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,Req_Code)
+    }
+
+    // onActivityResult() function : this is where we provide the task and data for the Google Account
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==Req_Code){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    // handleResult() function -  this is where we update the UI after Google signin takes place
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? =completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // UpdateUI() function - this is where we specify what UI updates are needed after google signin has taken place.
+    private fun UpdateUI(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task->
+            if(task.isSuccessful) {
+                GoogleLoginSavedPreferencesObject.setEmail(this,account.email.toString())
+                GoogleLoginSavedPreferencesObject.setUsername(this,account.displayName.toString())
+                val intent = Intent(this, TBMainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+            startActivity(Intent(this, TBMainActivity::class.java))
+            finish()
+        }
+    }
+
+
 }
