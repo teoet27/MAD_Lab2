7afbf2594de2ade40d52205ba2ec36634b2bbce1
annaoriglia <48447099+annaoriglia@users.noreply.github.com>
2022-05-17 21:40:01 +0200

    MAYBE auth works now. Preliminary test.

--- app/src/main/java/it/polito/MAD/group06/activities/TBMainActivity.kt
+++ app/src/main/java/it/polito/MAD/group06/activities/TBMainActivity.kt
@@ -45,7 +45,7 @@ class TBMainActivity : AppCompatActivity() {
 
         // configure the GoogleSignInOptions with the same server client ID used for logging in
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id_bis))
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         mGoogleSignInClient= GoogleSignIn.getClient(this, gso)
