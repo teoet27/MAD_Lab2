552ff0b3eb7689815e93cc1da0487e12ffbe45ee
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-21 12:06:25 +0200

    zeroed: ShowMyListOfAdvertisement.kt

---
+++ app/src/main/java/it/polito/madcourse/group06/views/timeslot/ShowMyListOfAdvertisement.kt
@@ -0,0 +1,23 @@
+package it.polito.madcourse.group06.views.timeslot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import it.polito.madcourse.group06.R
+
+class ShowMyListOfAdvertisement : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.show_my_list_of_advertisement, container, false)
+    }
+
+}
