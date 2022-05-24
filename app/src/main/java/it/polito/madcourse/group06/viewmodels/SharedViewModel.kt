02b0e3e4ae708d2e6810fecb10d80c3cc27af5b1
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-24 17:39:34 +0200

    minor changes

--- app/src/main/java/it/polito/madcourse/group06/viewmodels/SharedViewModel.kt
+++ app/src/main/java/it/polito/madcourse/group06/viewmodels/SharedViewModel.kt
@@ -4,39 +4,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import it.polito.madcourse.group06.utilities.TimeslotTools
 
-
 class SharedViewModel : ViewModel() {
-    private var _selected_skill = ""
-    private var _selected = false
-    private var _sortParam = "Title"
-    private var _sortUp = false
-    private var _lastSortUp = false
-    private var _filter = TimeslotTools.AdvFilter()
-    private var _updateRV = false
 
-    var selected_skill = MutableLiveData<String>().also {
-        it.value = _selected_skill
-    }
+    private var _selected = false
+    private var _filter = TimeslotTools.AdvFilter()
+
     var selected = MutableLiveData<Boolean>()
-    var sortParam = MutableLiveData<String>().also {
-        it.value = _sortParam
-    }
-    var sortUp = MutableLiveData<Boolean>().also {
-        it.value = _sortUp
-    }
-    var lastSortUp = MutableLiveData<Boolean>().also {
-        it.value = _lastSortUp
-    }
     var filter = MutableLiveData<TimeslotTools.AdvFilter>().also {
         it.value = _filter
-    }
-    var updateRV = MutableLiveData<Boolean>().also {
-        it.value = _updateRV
-    }
-
-    fun selectSkill(skill: String) {
-        _selected_skill = skill
-        selected_skill.value = _selected_skill
     }
 
     fun select(value: Boolean) {
@@ -49,33 +24,4 @@ class SharedViewModel : ViewModel() {
         filter.value = _filter
     }
 
-    fun setSortParam(name: String) {
-        _sortParam = name
-        sortParam.value = _sortParam
-    }
-
-    fun getSortParam(): String {
-        return sortParam.value!!
-    }
-
-    fun updateSort() {
-        sortUp.value = _sortUp
-    }
-
-    fun toggleSortDirection() {
-        _lastSortUp = _sortUp
-        lastSortUp.value = _lastSortUp
-        _sortUp = !_sortUp
-        sortUp.value = _sortUp
-    }
-
-    fun setSortUp() {
-        _sortUp = true
-        sortUp.value = _sortUp
-    }
-
-    fun updateRV() {
-        _updateRV = true
-        updateRV.value = _updateRV
-    }
 }
