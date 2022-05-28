b533f3c6ba8d71f5b52b2cd507245407bb259582
Matteo <teoet27@gmail.com>
2022-05-28 19:07:37 +0200

    fixed: errors in navigation to other user profile pt.2

--- app/src/main/java/it/polito/madcourse/group06/views/timeslot/ShowListOfTimeslots.kt
+++ app/src/main/java/it/polito/madcourse/group06/views/timeslot/ShowListOfTimeslots.kt
@@ -174,8 +174,18 @@ class ShowListOfTimeslots : Fragment(R.layout.show_timeslots_frag) {
             fullListForGivenSkill =
                 listOfAdv.filter { it.listOfSkills.contains(selectedSkill) || selectedSkill == "All" }
 
-            // Close filter window in case it was left open
+            // Close filter fragment in case it was left open
             activity?.supportFragmentManager?.findFragmentByTag("filter_window")?.also { frag ->
+                activity?.supportFragmentManager?.beginTransaction()?.remove(frag)?.commit()
+                sharedViewModel.select(false)
+            }
+            // Close single timeslot fragment in case it was left open
+            activity?.supportFragmentManager?.findFragmentByTag("single_timeslot")?.also { frag ->
+                activity?.supportFragmentManager?.beginTransaction()?.remove(frag)?.commit()
+                sharedViewModel.select(false)
+            }
+            // Close other user profile fragment in case it was left open
+            activity?.supportFragmentManager?.findFragmentByTag("other_user_profile")?.also { frag ->
                 activity?.supportFragmentManager?.beginTransaction()?.remove(frag)?.commit()
                 sharedViewModel.select(false)
             }
