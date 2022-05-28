3147d2ceeb587236c5fc1c30b5d6ac49711467f2
annaoriglia <48447099+annaoriglia@users.noreply.github.com>
2022-05-28 11:56:44 +0200

    Improved rating page.

--- app/src/main/java/it/polito/madcourse/group06/views/RatingFragment.kt
+++ app/src/main/java/it/polito/madcourse/group06/views/RatingFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RatingBar
-import android.widget.Toast
+import android.widget.RatingBar.OnRatingBarChangeListener
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -28,11 +28,28 @@ class RatingFragment: Fragment() {
         val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
         val submitRatingButton = view.findViewById<Button>(R.id.submitRating)
 
+        ratingBar.setRating(0F)
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            if (rating <= 0) {
+                // user is forbidden to vote 0 stars
+                ratingBar.setRating(0.5F)
+            }
+        }
+
         submitRatingButton?.setOnClickListener {
             val msg = ratingBar.rating.toString()
-            Snackbar.make(
-                requireView(), "Rating is: " + msg, Snackbar.LENGTH_LONG
-            ).show()
+
+            if (ratingBar.rating == 0F) {
+                Snackbar.make(
+                    requireView(), "Please vote your experience.", Snackbar.LENGTH_LONG
+                ).show()
+            }
+            else {
+                // TODO: do something with the rating
+                Snackbar.make(
+                    requireView(), "Rating is: " + msg, Snackbar.LENGTH_LONG
+                ).show()
+            }
         }
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
