9a4b935e2a9f9614990993b5471ffe1c54250b4f
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-05-31 18:40:25 +0200

    feat: clicking on a message will show/hide it's timestamp

--- app/src/main/java/it/polito/madcourse/group06/models/mychat/MyChatViewHolder.kt
+++ app/src/main/java/it/polito/madcourse/group06/models/mychat/MyChatViewHolder.kt
@@ -17,23 +17,29 @@ class MyChatViewHolder(private val v: View, private val isMyMessage: Boolean) :
         if (isMyMessage) {
             if (itemViewType == R.layout.my_message_layout) {
                 v.findViewById<LinearLayout>(R.id.myMainContainerID).gravity = Gravity.START
+                msgTimestamp = v.findViewById(R.id.myTimestampID)
                 msgContent = v.findViewById(R.id.myMessageTextViewID)
+                msgTimestamp.text = msg.timestamp
                 msgContent.text = msg.msg
             } else if (itemViewType == R.layout.common_my_message_layout) {
                 v.findViewById<LinearLayout>(R.id.myCommonMainContainerID).gravity = Gravity.START
-                msgContent = v.findViewById(R.id.myCommonMessageTextViewID)
                 msgTimestamp = v.findViewById(R.id.commonMyTimestampID)
+                msgContent = v.findViewById(R.id.myCommonMessageTextViewID)
                 msgTimestamp.text = msg.timestamp
                 msgContent.text = msg.msg
             }
         } else {
             if (itemViewType == R.layout.other_message_layout) {
                 v.findViewById<LinearLayout>(R.id.otherMainContainerID).gravity = Gravity.END
+                msgTimestamp = v.findViewById(R.id.otherTimestampID)
                 msgContent = v.findViewById(R.id.otherMessageTextViewID)
+                msgTimestamp.text = msg.timestamp
                 msgContent.text = msg.msg
             } else if (itemViewType == R.layout.common_other_message_layout) {
                 v.findViewById<LinearLayout>(R.id.otherCommonMainContainerID).gravity = Gravity.END
+                msgTimestamp = v.findViewById(R.id.commonOtherTimestampID)
                 msgContent = v.findViewById(R.id.otherCommonMessageTextViewID)
+                msgTimestamp.text = msg.timestamp
                 msgContent.text = msg.msg
             }
         }
