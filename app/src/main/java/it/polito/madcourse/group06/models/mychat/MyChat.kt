75f1d05e91742046afcb4f4e1cfd435d208e64d2
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-06-01 12:38:36 +0200

    updated: model

--- app/src/main/java/it/polito/madcourse/group06/models/mychat/MyChat.kt
+++ app/src/main/java/it/polito/madcourse/group06/models/mychat/MyChat.kt
@@ -1,5 +1,8 @@
 package it.polito.madcourse.group06.models.mychat
 
+import it.polito.madcourse.group06.viewmodels.MyMessage
+
 data class MyChat(
     var chatID: String,
+    var chatContent: List<MyMessage>
 )
