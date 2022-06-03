b74f35a99254809d1291d10c80c22b2b0de829bb
Matteo <teoet27@gmail.com>
2022-06-03 17:58:48 +0200

    solved problems about active ads management

--- app/src/main/java/it/polito/madcourse/group06/views/RatingFragment.kt
+++ app/src/main/java/it/polito/madcourse/group06/views/RatingFragment.kt
@@ -15,30 +15,37 @@ import androidx.fragment.app.activityViewModels
 import com.google.android.material.snackbar.Snackbar
 import it.polito.madcourse.group06.R
 import it.polito.madcourse.group06.activities.TBMainActivity
-import it.polito.madcourse.group06.models.advertisement.Advertisement
-import it.polito.madcourse.group06.models.userprofile.UserProfile
 import it.polito.madcourse.group06.viewmodels.AdvertisementViewModel
 import it.polito.madcourse.group06.viewmodels.UserProfileViewModel
 
-class RatingFragment: Fragment() {
+class RatingFragment : Fragment() {
     private lateinit var activityTB: TBMainActivity
 
     private val userProfileViewModel by activityViewModels<UserProfileViewModel>()
     private val advertisementViewModel by activityViewModels<AdvertisementViewModel>()
 
-    private var otherId: String? = ""
+    private var rxUserId: String? = ""
     private var advId: String? = ""
+    private var advAccountId: String? = ""
     private var advTitle: String? = ""
+    private var isServiceDone: Boolean = false
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        otherId = arguments?.getString("other_id")
+        rxUserId = arguments?.getString("rx_user_id")
+        advAccountId = arguments?.getString("adv_account_id")
+
         advId = arguments?.getString("adv_id")
         advTitle = arguments?.getString("adv_title")
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         activityTB = requireActivity() as TBMainActivity
         return inflater.inflate(R.layout.rating_fragment, container, false)
     }
@@ -55,7 +62,6 @@ class RatingFragment: Fragment() {
         val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
         val submitRatingButton = view.findViewById<Button>(R.id.submitRating)
 
-        var isServiceDone: Boolean = true
 
         ratingBar.setRating(0F)
         ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
@@ -65,54 +71,65 @@ class RatingFragment: Fragment() {
             }
         }
 
-        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (ratingBar.rating == 0F) {
-                    // go back to timeslots list
-                    val frag = activity?.supportFragmentManager!!.findFragmentByTag("rating_fragment")
-                    activity?.supportFragmentManager?.beginTransaction()?.remove(frag!!)?.commit()
-                } else {
-                    submitRatingButton.performClick()
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (ratingBar.rating == 0F) {
+                        // go back to timeslots list
+                        val frag =
+                            activity?.supportFragmentManager!!.findFragmentByTag("rating_fragment")
+                        activity?.supportFragmentManager?.beginTransaction()?.remove(frag!!)
+                            ?.commit()
+                    } else {
+                        submitRatingButton.performClick()
+                    }
                 }
-            }
-        })
+            })
 
-        otherId?.let {
-            advId?.let { it1 ->
-                advertisementViewModel.fetchSingleAdvertisementById(it1)
-                userProfileViewModel.fetchUserProfileById(it)
+        advAccountId?.let {
+            rxUserId?.let {
+                advId?.let {
+                    advertisementViewModel.fetchSingleAdvertisementById(advId!!)
 
-                userProfileViewModel.currentUser.observe(this.viewLifecycleOwner) { currentUser ->
-                    userProfileViewModel.otherUser.observe(this.viewLifecycleOwner) { otherUser ->
-                        if (!otherUser.id.isNullOrEmpty()) {
-                            submitRatingButton?.setOnClickListener {
-                                if (ratingBar.rating == 0F) {
-                                    Snackbar.make(
-                                        requireView(),
-                                        "Please vote your experience.",
-                                        Snackbar.LENGTH_LONG
-                                    ).show()
-                                } else {
-                                    // save rating and comments
-                                    val rating = ratingBar.rating.toDouble()
-                                    val comment =
-                                        view.findViewById<TextView>(R.id.comment_rating).text.toString()
+                    userProfileViewModel.currentUser.observe(this.viewLifecycleOwner) { currentUser ->
+                        isServiceDone = (currentUser.id == advAccountId)
+                        if (isServiceDone) {
+                            userProfileViewModel.fetchUserProfileById(rxUserId)
+                        } else {
+                            userProfileViewModel.fetchUserProfileById(advAccountId)
+                        }
+                        userProfileViewModel.otherUser.observe(this.viewLifecycleOwner) { otherUser ->
+                            if (!otherUser.id.isNullOrEmpty()) {
+                                submitRatingButton?.setOnClickListener {
+                                    if (ratingBar.rating == 0F) {
+                                        Snackbar.make(
+                                            requireView(),
+                                            "Please vote your experience.",
+                                            Snackbar.LENGTH_LONG
+                                        ).show()
+                                    } else {
+                                        // save rating and comments
+                                        val rating = ratingBar.rating.toDouble()
+                                        val comment =
+                                            view.findViewById<TextView>(R.id.comment_rating).text.toString()
 
-                                    userProfileViewModel.commentAd(
-                                        advTitle,
-                                        comment,
-                                        rating,
-                                        isServiceDone
-                                    )
+                                        userProfileViewModel.commentAd(
+                                            advTitle,
+                                            comment,
+                                            rating,
+                                            isServiceDone
+                                        )
 
-                                    advertisementViewModel.deactivateAd(isServiceDone)
+                                        advertisementViewModel.deactivateAd(isServiceDone)
 
-                                    // go back to timeslots list
-                                    val frag =
-                                        activity?.supportFragmentManager!!.findFragmentByTag("rating_fragment")
-                                    activity?.supportFragmentManager?.beginTransaction()
-                                        ?.remove(frag!!)
-                                        ?.commit()
+                                        // go back to timeslots list
+                                        val frag =
+                                            activity?.supportFragmentManager!!.findFragmentByTag("rating_fragment")
+                                        activity?.supportFragmentManager?.beginTransaction()
+                                            ?.remove(frag!!)
+                                            ?.commit()
+                                    }
                                 }
                             }
                         }
