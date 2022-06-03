af721cd5a895ff314aad5d674cb8e50dcb468430
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-06-03 20:01:21 +0200

    added: property name

--- app/src/main/java/it/polito/madcourse/group06/models/mychat/MyMessage.kt
+++ app/src/main/java/it/polito/madcourse/group06/models/mychat/MyMessage.kt
@@ -1,12 +1,27 @@
-package it.polito.madcourse.group06.viewmodels
+package it.polito.madcourse.group06.models.mychat
 
 import androidx.room.Entity
+import com.google.firebase.firestore.PropertyName
 
 @Entity(tableName = "mychatTable")
 data class MyMessage (
+    @get:PropertyName("sender_id")
+    @set:PropertyName("sender_id")
     var senderID: String,
+
+    @get:PropertyName("receiver_id")
+    @set:PropertyName("receiver_id")
     var receiverID: String,
+
+    @get:PropertyName("msg")
+    @set:PropertyName("msg")
     var msg: String,
+
+    @get:PropertyName("timestamp")
+    @set:PropertyName("timestamp")
     var timestamp: String,
-    var isAPurpose: Boolean
+
+    @get:PropertyName("is_an_offer")
+    @set:PropertyName("is_an_offer")
+    var isAnOffer: Boolean
 )
