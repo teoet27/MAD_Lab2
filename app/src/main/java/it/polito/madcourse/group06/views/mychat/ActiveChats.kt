bb420abcd0f838ff10b3153d74505351293681d9
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-06-04 11:25:31 +0200

    added: listOfActiveChats

--- app/src/main/java/it/polito/madcourse/group06/views/mychat/ActiveChats.kt
+++ app/src/main/java/it/polito/madcourse/group06/views/mychat/ActiveChats.kt
@@ -11,7 +11,6 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import it.polito.madcourse.group06.R
-import it.polito.madcourse.group06.models.advertisement.Advertisement
 import it.polito.madcourse.group06.models.mychat.ActiveChat
 import it.polito.madcourse.group06.models.mychat.ActiveChatAdapter
 import it.polito.madcourse.group06.viewmodels.AdvertisementViewModel
@@ -21,8 +20,7 @@ import it.polito.madcourse.group06.viewmodels.UserProfileViewModel
 class ActiveChats : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var activeChatAdapter: ActiveChatAdapter
-    private val listOfChatsAsCustomer: ArrayList<ActiveChat> = arrayListOf()
-    private val listOfChatsAsProvider: ArrayList<ActiveChat> = arrayListOf()
+    private var listOfActiveChats: ArrayList<ActiveChat> = arrayListOf()
     private val myChatViewModel: MyChatViewModel by activityViewModels()
     private val userProfileViewModel: UserProfileViewModel by activityViewModels()
     private val advertisementViewModel: AdvertisementViewModel by activityViewModels()
@@ -39,34 +37,41 @@ class ActiveChats : Fragment() {
         this.recyclerView = view.findViewById(R.id.activeChatsRecyclerViewID)
         this.recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        activeChatAdapter = ActiveChatAdapter(listOfChatsAsCustomer)
+        activeChatAdapter = ActiveChatAdapter(listOfActiveChats)
         this.recyclerView.adapter = activeChatAdapter
 
         userProfileViewModel.currentUser.observe(viewLifecycleOwner) { currentUser ->
             advertisementViewModel.listOfAdvertisements.observe(viewLifecycleOwner) { listOfAdvertisement ->
                 myChatViewModel.listOfChats.observe(viewLifecycleOwner) { listOfChats ->
-                    var adv = Advertisement(
-                        "", "", "", arrayListOf(),
-                        "", "", "", "", 0.0, "",
-                        "", "", "", "", 0.0
-                    )
-                    for (chat in listOfChats) {
-                        //if (chat.userID == currentUser.id!!) {
-                        // as customer
-                        for (elem in listOfAdvertisement) {
-                            if (chat.advID == elem.id!!) {
-                                adv = elem
-                                break
+                    userProfileViewModel.listOfUsers.observe(viewLifecycleOwner) { listOfUsers ->
+                        for (chat in listOfChats) {
+                            if (chat.userID == currentUser.id!!) {
+                                for (adv in listOfAdvertisement) {
+                                    for (user in listOfUsers) {
+                                        if(user.id!! == chat.otherUserID) {
+                                            val activeChat = ActiveChat(adv.advTitle, user.fullName!!, chat.chatID)
+                                            if (adv.id!! == chat.advID && !listOfActiveChats.contains(activeChat)) {
+                                                listOfActiveChats.add(activeChat)
+                                            }
+                                        }
+                                    }
+                                }
+                            } else if (chat.otherUserID == currentUser.id!!) {
+                                for (adv in listOfAdvertisement) {
+                                    for (user in listOfUsers) {
+                                        if(user.id!! == chat.userID) {
+                                            val activeChat = ActiveChat(adv.advTitle, user.fullName!!, chat.chatID)
+                                            if (adv.id!! == chat.advID && !listOfActiveChats.contains(activeChat)) {
+                                                listOfActiveChats.add(activeChat)
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
-                        activeChatAdapter.addActiveChat(
-                            ActiveChat(
-                                adv.advTitle,
-                                adv.advAccount,
-                                chat.chatID
-                            )
-                        )
-                        //}
+                        activeChatAdapter = ActiveChatAdapter(listOfActiveChats)
+                        listOfActiveChats = arrayListOf()
+                        this.recyclerView.adapter = activeChatAdapter
                     }
                 }
             }
