db458f99f950866721c03260f5420561aadd72a5
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-06-04 14:16:59 +0200

    added: message if the user is creating an incomplete proposal

--- app/src/main/java/it/polito/madcourse/group06/views/mychat/MyChat.kt
+++ app/src/main/java/it/polito/madcourse/group06/views/mychat/MyChat.kt
@@ -9,10 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -186,6 +183,8 @@ class MyChat : Fragment() {
                 closeProposal()
                 resetProposalFields()
                 this.isAnswerMenuOpen = !this.isAnswerMenuOpen
+            } else {
+                Toast.makeText(requireContext(), "Incomplete proposal.", Toast.LENGTH_LONG).show()
             }
         }
 
